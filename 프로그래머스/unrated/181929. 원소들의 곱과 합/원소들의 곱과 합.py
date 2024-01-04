97b15636d6c86b5afd8290c9d50e04f526f88f1f
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-04 21:43:00 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,12 +1,8 @@
 def solution(num_list):
     temp1 = 1
-    temp2 = 0
+    temp2 = sum(num_list) * sum(num_list)
     
     for n in num_list:
         temp1 *= n
-        
-    for n in num_list:
-        temp2 += n
-    temp2 *= temp2
     
     return 1 if (temp1 < temp2) else 0
