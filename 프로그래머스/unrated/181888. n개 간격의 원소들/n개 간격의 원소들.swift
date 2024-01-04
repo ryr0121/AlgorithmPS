f2ceaa53bbe18cbd31161c911d332d51a2b2985d
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-05 01:38:09 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.swift
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.swift
@@ -2,10 +2,13 @@ import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
     var arr: [Int] = []
-    var i = 0
-    while i < num_list.count {
+    // var i = 0
+    // while i < num_list.count {
+    //     arr.append(num_list[i])
+    //     i += n
+    // }
+    for i in stride(from:0 ,to: num_list.count, by: n) {
         arr.append(num_list[i])
-        i += n
     }
     return arr
 }
