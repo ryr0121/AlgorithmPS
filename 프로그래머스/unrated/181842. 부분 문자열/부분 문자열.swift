0cea5aa0025299068b05992071feb7d62a3b15e0
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-04 19:26:47 +0900

    [level unrated] Title: 부분 문자열, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ str1:String, _ str2:String) -> Int {
+    var result = 0
+    var str2List = str2.map { return String($0) }
+    for i in 0..<(str2.count-str1.count + 1) {
+        var temp = ""
+        for q in 0..<str1.count { temp += str2List[i+q] }
+        if (temp == str1) { result = 1; break; }
+    }
+    return result
+}
