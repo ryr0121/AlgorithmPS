a5e26e0fc4e24f0e77fde1c57650c95cfe54efda
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-04 19:28:19 +0900

    [level unrated] Title: 부분 문자열, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/README.md
+++ 프로그래머스/unrated/181842. 부분 문자열/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 부분 문자열 - 181842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181842#) 
 
 ### 성능 요약
 
-메모리: 16.1 MB, 시간: 0.03 ms
+메모리: 16.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 19:26:46
+2024년 1월 4일 19:28:18
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.swift
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.swift
@@ -1,12 +1,13 @@
 import Foundation
 
 func solution(_ str1:String, _ str2:String) -> Int {
-    var result = 0
-    var str2List = str2.map { return String($0) }
-    for i in 0..<(str2.count-str1.count + 1) {
-        var temp = ""
-        for q in 0..<str1.count { temp += str2List[i+q] }
-        if (temp == str1) { result = 1; break; }
-    }
-    return result
+    // var result = 0
+    // var str2List = str2.map { return String($0) }
+    // for i in 0..<(str2.count-str1.count + 1) {
+    //     var temp = ""
+    //     for q in 0..<str1.count { temp += str2List[i+q] }
+    //     if (temp == str1) { result = 1; break; }
+    // }
+    // return result
+    return str2.contains(str1) ? 1 : 0
 }
