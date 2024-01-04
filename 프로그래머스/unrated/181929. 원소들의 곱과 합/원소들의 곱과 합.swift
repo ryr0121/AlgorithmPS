0cf498f741df98bab5d7e2c7046d3edc91041e6c
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-04 21:40:43 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.swift
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.swift
@@ -1,12 +1,16 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
-    var temp1 = 1
-    var temp2 = 0
+//     var temp1 = 1
+//     var temp2 = 0
     
-    for n in num_list { temp1 *= n }
-    for n in num_list { temp2 += n }
+//     for n in num_list { temp1 *= n }
+//     for n in num_list { temp2 += n }
+//     temp2 *= temp2
+    
+//     return (temp1 < temp2) ? 1 : 0
+    var temp1 = num_list.reduce(1, *)
+    var temp2 = num_list.reduce(0, +)
     temp2 *= temp2
-    
     return (temp1 < temp2) ? 1 : 0
 }
