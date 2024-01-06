2fdea4577b8e824fc319cc0cbe109077dbb223d3
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-06 19:36:22 +0900

    [level unrated] Title: 수 조작하기 1, Time: 4.21 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ n:Int, _ control:String) -> Int {
+    var result = n
+    control.map{ String($0) }
+    for c in control {
+        switch (c) {
+            case "w": result += 1
+            case "s": result -= 1
+            case "d": result += 10
+            case "a": result -= 10
+            default: continue
+        }
+    }
+    return result
+}
