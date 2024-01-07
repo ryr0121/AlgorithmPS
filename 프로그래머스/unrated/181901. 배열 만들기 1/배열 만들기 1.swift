8e2572a23335c34edcb49c3b21001ec7c71e244e
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-07 19:40:56 +0900

    [level unrated] Title: 배열 만들기 1, Time: 1.32 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ n:Int, _ k:Int) -> [Int] {
+    var result: [Int] = []
+    for i in 1...n {
+        if (i%k == 0) { result.append(i) }
+    }
+    return result
+}
