b25fcf99a50c9d032a60dd5ccb7934d91ff89fba
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-07 22:52:54 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.swift
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.swift
@@ -4,7 +4,7 @@ func solution(_ a:Int, _ b:Int) -> Int {
     if (a%2 == 0 && b%2 == 0) {
         return abs(a-b)
     } else if (a%2 != 0 && b%2 != 0) {
-        return Int(pow(Float(a), 2.0) + pow(Float(b), 2.0))
+        return (a*a + b*b)
     } else {
        return 2*(a+b)
     }
