a0099032d555d6f5629169cb92d9a5aa85942170
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-08 15:42:06 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.swift
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.swift
@@ -1,21 +1,23 @@
 import Foundation
 
 func solution(_ sides:[Int]) -> Int {
-    var maxIdx = -1
-    var max = 0
-    var sum = 0
-    var other:[Int] = []
+//     var maxIdx = -1
+//     var max = 0
+//     var sum = 0
+//     var other:[Int] = []
     
-    for i in 0..<sides.count {
-        if max < sides[i] { 
-            max = sides[i];
-            maxIdx = i 
-        }
-    }
-    for i in 0..<sides.count {
-        if (i != maxIdx) { other.append(sides[i]) }
-    }
+//     for i in 0..<sides.count {
+//         if max < sides[i] { 
+//             max = sides[i];
+//             maxIdx = i 
+//         }
+//     }
+//     for i in 0..<sides.count {
+//         if (i != maxIdx) { other.append(sides[i]) }
+//     }
     
-    other.map{ sum += Int($0) }
-    return sum > max ? 1 : 2
+//     other.map{ sum += Int($0) }
+//     return sum > max ? 1 : 2
+    var sideArr = sides.sorted()
+    return ((sideArr[0] + sideArr[1]) > sideArr[2]) ? 1 : 2
 }
