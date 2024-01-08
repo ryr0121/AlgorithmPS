4ad3cb0d7e015858aa100a7fd5b0134a1c2f38e4
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-08 15:40:24 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ sides:[Int]) -> Int {
+    var maxIdx = -1
+    var max = 0
+    var sum = 0
+    var other:[Int] = []
+    
+    for i in 0..<sides.count {
+        if max < sides[i] { 
+            max = sides[i];
+            maxIdx = i 
+        }
+    }
+    for i in 0..<sides.count {
+        if (i != maxIdx) { other.append(sides[i]) }
+    }
+    
+    other.map{ sum += Int($0) }
+    return sum > max ? 1 : 2
+}
