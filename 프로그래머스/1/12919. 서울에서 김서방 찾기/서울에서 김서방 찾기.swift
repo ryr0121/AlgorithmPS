74268fc24216bbe826a437faeaba19dd10d362b6
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-01-27 01:57:32 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
@@ -0,0 +1,7 @@
+func solution(_ seoul:[String]) -> String {
+    var result = -1
+    for i in 0..<seoul.count {
+        if seoul[i] == "Kim" { result = i }
+    }
+    return "김서방은 \(result)에 있다"
+}
