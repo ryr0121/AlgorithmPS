02b6d2cc2777f796d3cb2ec0da12f57252f718a1
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-02-05 11:59:39 +0900

    [Silver IV] Title: 로프, Time: 3984 ms, Memory: 38112 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,10 @@
+ropes = []
+
+for _ in range(int(input())):
+    ropes.append(int(input()))
+ropes.sort(reverse=True)
+
+possibleWeights = []
+for i in range(len(ropes)):
+    possibleWeights.append(ropes[i]*(i+1))
+print(max(possibleWeights))
