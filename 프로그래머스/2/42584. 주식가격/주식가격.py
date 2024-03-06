8d23d75aad5b8014f47b08e7c19ee2bb857e00c9
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-03-06 23:28:50 +0900

    [level 2] Title: 주식가격, Time: 147.60 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -3,7 +3,7 @@ def solution(prices):
     for i in range(len(prices)):
         c = 0
         for j in range(i+1, len(prices)):
-            if prices[j] < prices[i]: c += 1; break;
             c += 1
+            if prices[j] < prices[i]: break
         answer.append(c)
     return answer
