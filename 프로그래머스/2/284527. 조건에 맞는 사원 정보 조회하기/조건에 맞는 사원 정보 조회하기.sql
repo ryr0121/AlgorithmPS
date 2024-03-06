6d65f23ddcee4c10280cc3c2d12a9e0d6af23371
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-03-06 14:31:55 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,7 @@
+select SUM(SCORE) as SCORE, HR_EMPLOYEES.EMP_NO, EMP_NAME, POSITION, EMAIL
+    from HR_EMPLOYEES join HR_GRADE
+    on HR_EMPLOYEES.EMP_NO = HR_GRADE.EMP_NO
+    and HR_GRADE.YEAR = "2022"
+    group by HR_EMPLOYEES.EMP_NO
+    order by SCORE desc
+    limit 1
