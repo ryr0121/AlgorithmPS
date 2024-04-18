285740a57f0a630ce461ad79e49eaa634836c202
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-04-19 02:49:55 +0900

    Update 창고정리.swift

--- 개인/창고정리.swift
+++ 개인/창고정리.swift
@@ -3,10 +3,11 @@ import Foundation
 let l = Int(readLine()!)!
 var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
 let m = Int(readLine()!)!
+arr.sort()
 
 for _ in 0..<m {
-    arr.sort()
     arr[l-1] -= 1
     arr[0] += 1
+    arr.sort()
 }
-print(arr.max()!-arr.min()!)
+print(arr[l-1]-arr[0])
