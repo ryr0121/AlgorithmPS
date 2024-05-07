8d78851096245a37a88425c04d31aff2789becc9
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-05-07 16:54:40 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,4 @@
+select book_id, author_name, date_format(published_date,'%Y-%m-%d') published_date
+    from book left join author using(author_id)
+    where category = '경제'
+    order by published_date
