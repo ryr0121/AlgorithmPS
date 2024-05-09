bf40e5ee55dcc21b5848e5c4198aea6a09553ac5
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-05-10 00:58:47 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,7 @@
+select distinct cart_id from cart_products
+    where name = 'Milk'
+    and cart_id in (
+        select cart_id from cart_products
+        where name = 'Yogurt'
+    )
+    order by cart_id
