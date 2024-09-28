4aac7dc8ba4dd6b4734c3480729d3a71a98af30a
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-09-29 00:45:29 +0900

    [Gold V] Title: 선 긋기, Time: 552 ms, Memory: 174412 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.swift
@@ -0,0 +1,90 @@
+import Foundation
+
+let fIO = FileIO()
+
+let n = fIO.readInt()
+var lines:[[Int]] = []
+for _ in 0..<n {
+    var temp:[Int] = []
+    temp.append(fIO.readInt())
+    temp.append(fIO.readInt())
+    lines.append(temp)
+}
+lines.sort{ $0[0] < $1[0] }
+
+var start = lines[0][0]
+var end = lines[0][1]
+var answer = 0
+
+for i in 1..<n {
+    if (lines[i][0] > end) {
+        answer += end-start
+        start = lines[i][0]
+        end = lines[i][1]
+    } else if (lines[i][1] > end) {
+        end = lines[i][1]
+    }
+}
+answer += end-start
+print(answer)
+
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
