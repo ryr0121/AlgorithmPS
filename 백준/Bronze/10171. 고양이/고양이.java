be311c70c47e91e997e65083e592c63e09f2c816
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-10-07 17:33:37 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
