becefddd6d5cad4d5de6e70609a4d4270cf02027
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-11-04 14:54:52 +0900

    [Silver IV] Title: 스택, Time: 444 ms, Memory: 34832 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<N; i++) {
+            String cmd = sc.next();
+            switch(cmd) {
+                case "push":
+                    stack.push(sc.nextInt());
+                    break;
+                case "pop":
+                    System.out.println(stack.isEmpty() ? -1 : stack.pop());
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println(stack.isEmpty() ? 1 : 0);
+                    break;
+                case "top":
+                    System.out.println(stack.isEmpty() ? -1 : stack.peek());
+                    break;
+            }
+        }
+    }
+}
