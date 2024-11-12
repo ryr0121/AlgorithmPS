c7067e2bf4916e1041a158b50ecfd0ee7f9d6720
CHONI <49021999+ryr0121@users.noreply.github.com>
2024-11-13 01:04:45 +0900

    [Silver I] Title: 단지번호붙이기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
@@ -1,36 +1,38 @@
-import Foundation
+let N = Int(readLine()!)!
+var arr:[[Int]] = []
+var ch:[[Bool]] = Array(repeating: Array(repeating: false, count: N), count: N)
+for _ in 0..<N { arr.append(readLine()!.map{Int(String($0))!}) }
 
-let dx = [-1,0,1,0]
-let dy = [0,-1,0,1]
-func DFS(_ x: Int, _ y: Int) -> Void {
-    cnt += 1
-    board[x][y] = 0
-    for i in 0..<4 {
-        let nx = x + dx[i]
-        let ny = y + dy[i]
-        if nx > -1 && nx < n && ny > -1 && ny < n && board[nx][ny] == 1 {
-            DFS(nx,ny)
-        }
-    }
-}
-
-let n = Int(readLine()!)!
-var board:[[Int]] = []
 var cnt = 0
-var res:[Int] = []
-for _ in 0..<n {
-    board.append(readLine()!.map{Int(String($0))!})
-}
+var sizes:[Int] = []
 
-for i in 0..<n {
-    for j in 0..<n {
-        if board[i][j] == 1 {
-            cnt = 0
-            DFS(i,j)
-            res.append(cnt)
+var q:[[Int]] = []
+let dx = [-1,0,1,0]
+let dy = [0,1,0,-1]
+
+for i in 0..<N {
+    for j in 0..<N {
+        if(arr[i][j] == 1 && !ch[i][j]) {
+            cnt += 1
+            var size = 1
+            q.append([i,j])
+            ch[i][j] = true
+            while(!q.isEmpty) {
+                let cur = q.removeFirst()
+                for d in 0..<4 {
+                    let nx = cur[0] + dx[d]
+                    let ny = cur[1] + dy[d]
+                    if(nx > -1 && nx < N && ny > -1 && ny < N && !ch[nx][ny] && arr[nx][ny] == 1) {
+                        q.append([nx,ny])
+                        ch[nx][ny] = true
+                        size += 1
+                    }
+                }
+            }
+            sizes.append(size)
         }
     }
 }
-res.sort()
-print(res.count)
-for i in 0..<res.count { print(res[i]) }
+print(cnt)
+sizes.sort()
+for size in sizes { print(size) }
