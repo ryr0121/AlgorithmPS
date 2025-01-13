dc48d1d85d57cedeb538857a42b6958487613f49
CHONI <49021999+ryr0121@users.noreply.github.com>
2025-01-13 22:47:35 +0900

    [level 0] Title: ad 제거하기, Time: 2.07 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(int i=0; i<strArr.length; i++) {
+            if(!strArr[i].contains("ad")) list.add(strArr[i]);
+        }
+        
+        String[] answer = new String[list.size()];
+        int idx = 0;
+        for(String str : list) answer[idx++] = str;
+        
+        return answer;
+    }
+}
