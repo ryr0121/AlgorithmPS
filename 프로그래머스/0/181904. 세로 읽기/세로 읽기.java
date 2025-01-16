dffa616ef08133ae4b6af438026a4165763221bc
CHONI <49021999+ryr0121@users.noreply.github.com>
2025-01-16 19:14:14 +0900

    [level 0] Title: 세로 읽기, Time: 3.08 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        for(int i=0; i<my_string.length()/m; i++) {
+            answer += Character.toString(my_string.charAt(c-1 + m*i));
+        }
+        return answer;
+    }
+}
